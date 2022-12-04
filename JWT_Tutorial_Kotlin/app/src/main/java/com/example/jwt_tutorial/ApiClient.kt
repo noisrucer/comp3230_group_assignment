4fded0249fee82c55a82f5ccdf10c01aa4cc0579
Jason Lee <changjin9792@gmail.com>
2022-12-04 22:19:40 +0800

    integrate

--- JWT_Tutorial_Kotlin/app/src/main/java/com/example/jwt_tutorial/ApiClient.kt
+++ JWT_Tutorial_Kotlin/app/src/main/java/com/example/jwt_tutorial/ApiClient.kt
@@ -6,7 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "http://localhost:8000"
+const val BASE_URL = "http://10.0.2.2:8000"
 private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
 /**
